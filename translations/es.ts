1eaaf92e991950da15065a9e95d5e815c8ccbd87
Leonardo Fabian Romero Perez <leoromero@MacBook-Pro-de-Leonardo.local>
2022-02-03 13:36:00 -0500

    setting some class in header and about me

--- translations/es.ts
+++ translations/es.ts
@@ -5,6 +5,6 @@ export default {
   ABOUT_ME_ONE:
     'Hola, soy Leonardo Romero, un apasionado desarrollador web autodidacta e ingeniero de sistemas de Colombia. Mi pasión por el software radica en soñar con ideas y hacerlas realidad con interfaces elegantes y la calidad del código de las cosas que construyo.',
   ABOUT_ME_TWO:
-    'Me gusta trabajar con personas que se preocupan más por las personas y trabajar juntas por un objetivoMe gusta trabajar con personas que se preocupan más por las personas y trabajar juntas por un objetivo.',
+    'Me gusta trabajar con personas que se preocupan más por las personas y trabajar juntas por un objetivo Me gusta trabajar con personas que se preocupan más por las personas y trabajar juntas por un objetivo.',
   ABOUT_ME_THREE: 'Made in Cúcuta and deployed in Medellín'
 };
