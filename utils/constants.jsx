c684269d7cdb541b60100b1bc00325016422cb30
Leonardo Fabian Romero Perez <leoromero@MacBook-Pro-de-Leonardo.local>
2022-02-09 13:48:36 -0500

    added atribute alt in img

--- utils/constants.jsx
+++ utils/constants.jsx
@@ -1,10 +1,21 @@
 export const LIST_CONTACT = [
-  { href: 'https://twitter.com/leofrp', src: '/images/twitter_black.png', key: 'twitter' },
-  { href: 'https://github.com/leonardofrp5', src: '/images/github_black.png', key: 'github' },
+  {
+    href: 'https://twitter.com/leofrp',
+    src: '/images/twitter_black.png',
+    key: 'twitter',
+    alt: 'logo-twitter'
+  },
+  {
+    href: 'https://github.com/leonardofrp5',
+    src: '/images/github_black.png',
+    key: 'github',
+    alt: 'logo-github'
+  },
   {
     href: 'https://www.linkedin.com/in/leonardo-fabian-romero-perez-516479168/',
     src: '/images/linkedin_black.png',
-    key: 'linkedin'
+    key: 'linkedin',
+    alt: 'logo-linkedin'
   }
 ];
 
